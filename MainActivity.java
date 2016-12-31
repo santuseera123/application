b79eac5e83f573276045d8760f6bb1f5e85234fe
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:36:31 +0530

    intent_Tutorial Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,50 +1,39 @@
-package myapplicationishello.com.example.hsport.storage;
+package myapplicationishello.com.example.hsport.intent_tutorial;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
-    SharedPreferences.Editor spe;
-    EditText et1,et2,et3,et4,et5,et6;
-    SharedPreferences spf;
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spf=getSharedPreferences("myprefs", Context.MODE_PRIVATE);
-        spe=spf.edit();
-         et1=(EditText)findViewById(R.id.editText);
-         et2=(EditText)findViewById(R.id.editText2);
-         et3=(EditText)findViewById(R.id.editText3);
-         et4=(EditText)findViewById(R.id.editText4);
-         et5=(EditText)findViewById(R.id.editText5);
-         et6=(EditText)findViewById(R.id.editText6);
-    }
 
-    public void login(View v){
-        String name=spf.getString("uname","novalue");
-        String pass=spf.getString("pass","novalue");
-        if(et1.getText().toString().equalsIgnoreCase(name)&&et2.getText().toString().equalsIgnoreCase(pass)){
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
 
-            Toast.makeText(getApplicationContext(),"successs",Toast.LENGTH_LONG).show();
-        }else {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://www.example.com"));
+                startActivity(i);
+            }
+        });
 
-            Toast.makeText(getApplicationContext(),"INVALID",Toast.LENGTH_LONG).show();
-        }
-    }
-    public  void register(View v){
-
-        SharedPreferences.Editor spe=spf.edit();
-        spe.putString("uname",et3.getText().toString());
-        spe.putString("pass",et4.getText().toString());
-        spe.putString("Mobile No",et5.getText().toString());
-        spe.putString("eMail",et6.getText().toString());
-        spe.commit();
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("tel:9510300000"));
+                startActivity(i);
+            }
+        });
     }
 }
