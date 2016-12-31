941f9c53c156f5367e751132fd5597b126831551
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:10:48 +0530

    Wifi Test Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,21 +1,59 @@
-package myapplicationishello.com.example.hsport.qtp;
+package myapplicationishello.com.example.hsport.wifitest;
 
-import android.support.annotation.Nullable;
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    WifiManager wmanager;
+    Switch sw1;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wmanager=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+        sw1=(Switch)findViewById(R.id.switch1);
+        int i=wmanager.getWifiState();
+
+        if(i==0){
+            sw1.setChecked(false);
+        }else  if(i==3){
+            sw1.setChecked(true);
+        }
+
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    wmanager.setWifiEnabled(true);
+                }else{
+                    wmanager.setWifiEnabled(false);
+                }
+            }
+        });
     }
-        public void button(View v) {
-        EditText et1 = (EditText) findViewById(R.id.et1);
-        TextView tv1 = (TextView) findViewById(R.id.tv1);
-        tv1.setText(tv1.getText()+et1.getText().toString()+"\n")
-    }
+        public void getscanresults(View v){
+            List<ScanResult>results=wmanager.getScanResults();
+            for(int i=0;i<results.size();i++){
+
+                ScanResult result=results.get(i);
+                Toast.makeText(getApplicationContext(),result.SSID+"/n"+result.frequency,2000).show();
+            }
+        }
+       public void  getconfigurednetworks(View v){
+           List<WifiConfiguration>configs=wmanager.getConfiguredNetworks();
+           for(int i=0;i<configs.size();i++){
+               WifiConfiguration result=configs.get(i);
+               Toast.makeText(getApplicationContext(),result.SSID+"/n"+result.status,2000).show();
+           }
+       }
 }
