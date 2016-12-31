3ccb750c0976df602fc553485cd8394a308f4366
santuseera123 <santu.seera123@gmail.com>
2016-12-31 16:57:48 +0530

    Bluetooth Test Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,32 +1,36 @@
-package myapplicationishello.com.example.hsport.contents;
+package myapplicationishello.com.example.hsport.bttest;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch sw1;
+    BluetoothAdapter bAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void dial(View v){
-        Intent i=new Intent();
-        i.setAction(Intent.ACTION_DIAL);
-        EditText et1=(EditText) findViewById(R.id.et1);
-        i.setData(Uri.parse("tel:"+et1.getText()));
-        startActivity(i);
-    }
-    public void next(View v){
-        Intent i=new Intent();
-        i.setComponent(new ComponentName(getApplicationContext(),Welcome.class));
-        startActivity(i);
-    }
+        sw1=(Switch)findViewById(R.id.switch1);
+        bAdapter=BluetoothAdapter.getDefaultAdapter();
+        sw1.setChecked(bAdapter.isEnabled());
+        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    bAdapter.enable();
+                 }
+                else{
+                    bAdapter.disable();
+                 }
+             }
+        });
+      }
+
+       public void getbtdevices(View v){
+           bAdapter.startDiscovery();
+       }
 }
