fb4ae32fe743113be13eb06c457adca6be0faf92
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:09:18 +0530

    Messanger Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,43 +1,21 @@
-package myapplicationishello.com.example.hsport.toaccess;
+package myapplicationishello.com.example.hsport.qtp;
 
-import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public  void twitter(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.twitter.android");
-        startActivity(i);
-    }
-    public  void whatsapp(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.whatsapp");
-        startActivity(i);
-    }
-    public  void mxplayer(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.mxtech.videoplayer.ad");
-        startActivity(i);
-    }
-    public  void shareit(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.lenovo.anyshare.gps");
-        startActivity(i);
-    }
-    public  void youtube(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.google.android.youtube");
-        startActivity(i);
-    }
-    public  void facebook(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.facebook.katana");
-        startActivity(i);
-    }
-    public  void ucbrowser(View v){
-        Intent i=getPackageManager().getLaunchIntentForPackage("com.UCMobile.intl");
-        startActivity(i);
+        public void button(View v) {
+        EditText et1 = (EditText) findViewById(R.id.et1);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        tv1.setText(tv1.getText()+et1.getText().toString()+"\n")
     }
 }
