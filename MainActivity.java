96c7d8660d7498afa63e276f619200540655b6b8
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:21:01 +0530

    android MAP application

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,33 @@
-package myapplicationishello.com.example.hsport.jsontest;
+package myapplicationishello.com.example.hsport.map;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity {
+
+    GoogleMap gmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment frag=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+        frag.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+                gmap=googleMap;
+
+            }
+        });
+
+
     }
 }
