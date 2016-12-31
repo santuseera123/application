d6b5bc529de08b1f49100de606e7411d31fe5435
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:39:15 +0530

    Calculator Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,67 @@
-package myapplicationishello.com.example.hsport.intent_tutorial;
+package myapplicationishello.com.example.hsport.santurbk;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    Button b1,b2;
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
+    String total="";
+    double v1,v2;
+    String sign="";
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void onClick(View v){
 
-        b1=(Button)findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
+        Button button=(Button)v;
+        String str=button.getText().toString();
+        total+=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText(total);
+    }
 
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
-                        Uri.parse("http://www.example.com"));
-                startActivity(i);
-            }
-        });
+    public  void onAdd(View v){
 
-        b2=(Button)findViewById(R.id.button2);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
-                        Uri.parse("tel:9510300000"));
-                startActivity(i);
-            }
-        });
+        v1=Double.parseDouble(total);
+        total="";
+        Button button=(Button)v;
+        String str=button.getText().toString();
+        sign=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
+    }
+    public  void onCalculate(View v){
+
+        EditText edit=(EditText)findViewById(R.id.editText);
+        String str2=edit.getText().toString();
+        v2=Double.parseDouble(str2);
+        double grand_total=0;
+        if(sign.equals("+")){
+            grand_total=v1+v2;
+        }
+        else  if(sign.equals("-")){
+            grand_total=v1-v2;
+        }
+        else if(sign.equals("*")){
+            grand_total=v1*v2;
+        }
+        else  if(sign.equals("/")){
+            grand_total=v1/v2;
+        }
+        else  if(sign.equals("%")){
+            grand_total=v1%v2;
+        }
+        edit.setText(grand_total+"");
+    }
+    public  void onClear(View v){
+
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
+        total="";
     }
 }
