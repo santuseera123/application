03be7de2e96e393675eaab5cde586ac397edbb1b
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:22:44 +0530

    Calculator Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,33 +1,67 @@
-package myapplicationishello.com.example.hsport.map;
+package myapplicationishello.com.example.hsport.santurbk;
 
-import android.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+public class MainActivity extends Activity {
 
-public class MainActivity extends FragmentActivity {
-
-    GoogleMap gmap;
-
-    @Override
+    String total="";
+    double v1,v2;
+    String sign="";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment frag=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.fragment);
+    }
+    public  void onClick(View v){
 
-        frag.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
+        Button button=(Button)v;
+        String str=button.getText().toString();
+        total+=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText(total);
+    }
 
-                gmap=googleMap;
+    public  void onAdd(View v){
 
-            }
-        });
+        v1=Double.parseDouble(total);
+        total="";
+        Button button=(Button)v;
+        String str=button.getText().toString();
+        sign=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
+    }
+    public  void onCalculate(View v){
 
+        EditText edit=(EditText)findViewById(R.id.editText);
+        String str2=edit.getText().toString();
+        v2=Double.parseDouble(str2);
+        double grand_total=0;
+        if(sign.equals("+")){
+            grand_total=v1+v2;
+        }
+        else  if(sign.equals("-")){
+            grand_total=v1-v2;
+        }
+        else if(sign.equals("*")){
+            grand_total=v1*v2;
+        }
+        else  if(sign.equals("/")){
+            grand_total=v1/v2;
+        }
+        else  if(sign.equals("%")){
+            grand_total=v1%v2;
+        }
+        edit.setText(grand_total+"");
+    }
+    public  void onClear(View v){
 
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
+        total="";
     }
 }
