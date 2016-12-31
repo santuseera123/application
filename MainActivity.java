b25f998f514b4fe15b7e115a598d5d1a3d75f991
santuseera123 <santu.seera123@gmail.com>
2016-12-31 17:18:39 +0530

    JSON test Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,7 @@
-package myapplicationishello.com.example.hsport.time_picker;
+package myapplicationishello.com.example.hsport.jsontest;
 
-import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,16 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void time(View v){
-        TimePickerDialog.OnTimeSetListener listener=new TimePickerDialog.OnTimeSetListener(){
-            @Override
-            public void onTimeSet(TimePicker timePicker, int hourOfDay, int Minutes) {
-                TextView tv=(TextView)findViewById(R.id.textView);
-                tv.setText(hourOfDay+"-"+Minutes);
-            }
-        };
-        TimePickerDialog tpd=new TimePickerDialog(this,listener,10,50,false);
-        tpd.show();
     }
 }
