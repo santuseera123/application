a8db643eb181029149a06af0e79e3fac08cf9d9f
santuseera123 <santu.seera123@gmail.com>
2017-01-06 21:55:03 +0530

    Calculator Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,37 +1,67 @@
-package myapplicationishello.com.example.hsport.fragementconcept;
+package myapplicationishello.com.example.hsport.santurbk;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    android.app.FragmentTransaction tx;
-    FragmentManager fmanager;
+public class MainActivity extends Activity {
 
-    @Override
+    String total="";
+    double v1,v2;
+    String sign="";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public  void onClick(View v){
 
-    public  void home(View v){
-        FragmentManager fmanager=getFragmentManager();
-        tx=fmanager.beginTransaction();
-        tx.replace(R.id.fragment,new Homefragement());
-        tx.commit();
+        Button button=(Button)v;
+        String str=button.getText().toString();
+        total+=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText(total);
     }
-    public  void gallery(View v){
-        FragmentManager fmanager=getFragmentManager();
-        tx=fmanager.beginTransaction();
-        tx.replace(R.id.fragment,new Galleryfragement());
-        tx.commit();
+
+    public  void onAdd(View v){
+
+        v1=Double.parseDouble(total);
+        total="";
+        Button button=(Button)v;
+        String str=button.getText().toString();
+        sign=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
     }
-    public void contactus(View v){
-        FragmentManager fmanager=getFragmentManager();
-        tx=fmanager.beginTransaction();
-        tx.replace(R.id.fragment,new Contactusfragement());
+    public  void onCalculate(View v){
+
+        EditText edit=(EditText)findViewById(R.id.editText);
+        String str2=edit.getText().toString();
+        v2=Double.parseDouble(str2);
+        double grand_total=0;
+        if(sign.equals("+")){
+            grand_total=v1+v2;
+        }
+        else  if(sign.equals("-")){
+            grand_total=v1-v2;
+        }
+        else if(sign.equals("*")){
+            grand_total=v1*v2;
+        }
+        else  if(sign.equals("/")){
+            grand_total=v1/v2;
+        }
+        else  if(sign.equals("%")){
+            grand_total=v1%v2;
+        }
+        edit.setText(grand_total+"");
+    }
+    public  void onClear(View v){
+
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
+        total="";
     }
 }
