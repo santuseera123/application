445cdd00b123d3be4e56bbec0a356aa3ce4a57c5
santuseera123 <santu.seera123@gmail.com>
2017-01-06 21:42:44 +0530

    Messaging Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,32 +1,13 @@
-package myapplicationishello.com.example.hsport.audiorecordingtest;
+package myapplicationishello.com.example.hsport.appmessaging;
 
-import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-   MediaRecorder recorder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recorder=new MediaRecorder();
-        recorder=new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setOutputFile("/storage/sdcard0/obb.amr");
     }
-      public void start(View v){
-      try {
-          recorder.prepare();
-          recorder.start();
-      }catch (Exception e){
-      }
-      }
-     public void stop(View v){
-
-         recorder.stop();
-     }
 }
