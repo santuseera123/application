7553b2fc1fd297dec0c2405f7ae757a98b216f9a
santuseera123 <santu.seera123@gmail.com>
2017-01-06 22:01:03 +0530

    google_map_application

--- MainActivity.java
+++ MainActivity.java
@@ -1,59 +1,33 @@
-package myapplicationishello.com.example.hsport.wifitest;
+package myapplicationishello.com.example.hsport.map;
 
-import android.content.Context;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiManager;
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.Toast;
 
-import java.util.List;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
-    WifiManager wmanager;
-    Switch sw1;
+public class MainActivity extends FragmentActivity {
+
+    GoogleMap gmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        wmanager=(WifiManager)getSystemService(Context.WIFI_SERVICE);
-        sw1=(Switch)findViewById(R.id.switch1);
-        int i=wmanager.getWifiState();
+        SupportMapFragment frag=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.fragment);
 
-        if(i==0){
-            sw1.setChecked(false);
-        }else  if(i==3){
-            sw1.setChecked(true);
-        }
-
-        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        frag.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    wmanager.setWifiEnabled(true);
-                }else{
-                    wmanager.setWifiEnabled(false);
-                }
+            public void onMapReady(GoogleMap googleMap) {
+
+                gmap=googleMap;
+
             }
         });
-    }
-        public void getscanresults(View v){
-            List<ScanResult>results=wmanager.getScanResults();
-            for(int i=0;i<results.size();i++){
 
-                ScanResult result=results.get(i);
-                Toast.makeText(getApplicationContext(),result.SSID+"/n"+result.frequency,2000).show();
-            }
-        }
-       public void  getconfigurednetworks(View v){
-           List<WifiConfiguration>configs=wmanager.getConfiguredNetworks();
-           for(int i=0;i<configs.size();i++){
-               WifiConfiguration result=configs.get(i);
-               Toast.makeText(getApplicationContext(),result.SSID+"/n"+result.status,2000).show();
-           }
-       }
+
+    }
 }
