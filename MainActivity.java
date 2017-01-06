f3d91652b0d88d224ded23448bdb3662eb895be1
santuseera123 <santu.seera123@gmail.com>
2017-01-06 21:47:24 +0530

    Movie Choose Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,6 @@
-package myapplicationishello.com.example.hsport.countryselectedapp;
+package myapplicationishello.com.example.hsport.cinemachoice;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -15,8 +14,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         actv=(AutoCompleteTextView)findViewById(R.id.actv);
         String[] values=getResources().getStringArray(R.array.country_name);
-        ArrayAdapter<String>adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,
-                values);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.
+                      simple_spinner_dropdown_item,values);
         actv.setAdapter(adapter);
         actv.setThreshold(1);
     }
