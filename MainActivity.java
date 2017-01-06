09a247fe935c523eec89ccb888488b1a7aadab50
santuseera123 <santu.seera123@gmail.com>
2017-01-06 20:55:51 +0530

    scholarshipApp another model

--- MainActivity.java
+++ MainActivity.java
@@ -1,67 +1,62 @@
-package myapplicationishello.com.example.hsport.santurbk;
+package myapplicationishello.com.example.hsport.work;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-    String total="";
-    double v1,v2;
-    String sign="";
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends ListActivity implements android.view.View.OnClickListener{
+    Button btnAdd,btnGetAll;
+    TextView student_Id;
+    @Override
+    public void onClick(View view) {
+        if (view== findViewById(R.id.btnAdd)){
+            Intent intent = new Intent(this,StudentDetail.class);
+            intent.putExtra("student_Id",0);
+            startActivity(intent);
+        }else {
+
+            StudentRepo repo = new StudentRepo(this);
+
+            ArrayList<HashMap<String, String>> studentList =  repo.getStudentList();
+            if(studentList.size()!=0) {
+                ListView lv = getListView();
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        student_Id = (TextView) view.findViewById(R.id.student_Id);
+                        String studentId = student_Id.getText().toString();
+                        Intent objIndent = new Intent(getApplicationContext(),StudentDetail.class);
+                        objIndent.putExtra("student_Id", Integer.parseInt( studentId));
+                        startActivity(objIndent);
+                    }
+                });
+                ListAdapter adapter = new SimpleAdapter( MainActivity.this,studentList, R.layout.view_student_entry, new String[] { "id","name"}, new int[] {R.id.student_Id, R.id.student_name});
+                setListAdapter(adapter);
+            }else{
+                Toast.makeText(this,"No student!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+      @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public  void onClick(View v){
-
-        Button button=(Button)v;
-        String str=button.getText().toString();
-        total+=str;
-        EditText edit=(EditText)findViewById(R.id.editText);
-        edit.setText(total);
-    }
-
-    public  void onAdd(View v){
-
-        v1=Double.parseDouble(total);
-        total="";
-        Button button=(Button)v;
-        String str=button.getText().toString();
-        sign=str;
-        EditText edit=(EditText)findViewById(R.id.editText);
-        edit.setText("");
-    }
-    public  void onCalculate(View v){
-
-        EditText edit=(EditText)findViewById(R.id.editText);
-        String str2=edit.getText().toString();
-        v2=Double.parseDouble(str2);
-        double grand_total=0;
-        if(sign.equals("+")){
-            grand_total=v1+v2;
-        }
-        else  if(sign.equals("-")){
-            grand_total=v1-v2;
-        }
-        else if(sign.equals("*")){
-            grand_total=v1*v2;
-        }
-        else  if(sign.equals("/")){
-            grand_total=v1/v2;
-        }
-        else  if(sign.equals("%")){
-            grand_total=v1%v2;
-        }
-        edit.setText(grand_total+"");
-    }
-    public  void onClear(View v){
-
-        EditText edit=(EditText)findViewById(R.id.editText);
-        edit.setText("");
-        total="";
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+        btnGetAll = (Button) findViewById(R.id.btnGetAll);
+        btnGetAll.setOnClickListener(this);
     }
 }
