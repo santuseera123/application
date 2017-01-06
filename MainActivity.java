8fd5b0b3fb124e4348659ce73ed6d8b5c1945cd6
santuseera123 <santu.seera123@gmail.com>
2017-01-06 21:44:16 +0530

    Check Box Application

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,30 @@
-package myapplicationishello.com.example.hsport.appmessaging;
+package myapplicationishello.com.example.hsport.checkbox;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CheckBox checkBox=(CheckBox)findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"checked",Toast.LENGTH_LONG).show();
+
+                }else {
+                    Toast.makeText(getApplicationContext(),"un checked",Toast.LENGTH_LONG).show();
+                }
+
+                }
+        });
     }
 }
